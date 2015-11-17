19452c3182dd1294f317aabbefdf5f5cf4e244fd
Andrew Barbour <andrew.barbour1@gmail.com>
2015-11-18 01:24:28 +1100

    Committed Project

---
+++ Assets/Simple Text Dialogue System/Scripts/Dialogue.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class Dialogue : MonoBehaviour
+{
+    // Reference to Text component
+    Text _text;
+
+    // Reference to Dialogue System within the scene
+    DialogueSystem _dialogueSystem;
+
+    // Full dialogue message
+    string _dialogue;
+
+    // Position within the text string, for animating
+    int _textPosition;
+
+    public bool fullMessageShown
+    {
+        get
+        {
+            return _textPosition >= _dialogue.Length;
+        }
+    }
+
+    public void Awake()
+    {
+        // Get reference to Text component
+        _text = GetComponent<Text>();
+        if (_text == null)
+        {
+            Debug.LogError(string.Format("({0}) Unable to find Text component on this object", this.name));
+        }
+
+        // Get reference to Dialogue System
+        _dialogueSystem = FindObjectOfType<DialogueSystem>();
+        if (_dialogueSystem == null)
+        {
+            Debug.LogError(string.Format("({0}) Unable to find Dialogue System within scene", this.name));
+        }
+
+        // Save text within message
+        _dialogue = _text.text;
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnEnable()
+    {
+        // Used to prevent prefabs from calling Invokes
+        if (transform.parent == null)
+        {
+            return;
+        }
+
+        // Reset dialogue string, and position within text
+        _textPosition   = 0;
+
+        // Began animation of text
+        InvokeRepeating("AnimateText", 0.0f, _dialogueSystem.textSpeed);
+    }
+
+    public void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    void AnimateText()
+    {
+        // Show text
+        _text.text = _dialogue.Substring(0, _textPosition);
+
+        if (_textPosition < _dialogue.Length)
+        {
+            Debug.Log(string.Format("({0}) Animating text", this.name));
+            _textPosition++;
+        }
+        else
+        {
+            Debug.Log(string.Format("{0} is displaying end of dialogue prompt", this.name));
+
+            // Display End of Dialogue Prompt
+            _dialogueSystem.DisplayEndOfDialoguePrompt();
+
+            CancelInvoke();
+        }
+    }
+
+    public void ShowFullMessage()
+    {
+        _textPosition = _dialogue.Length;
+
+        CancelInvoke();
+        AnimateText();
+    }
+}
---
+++ Assets/Simple Text Dialogue System/Scripts/DialogueSystem.cs
@@ -0,0 +1,170 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class DialogueSystem : MonoBehaviour
+{
+    [Header("References")]
+    [Tooltip(@"Children of this object will be treated as dialogue. 
+        They will appear using the order within the hierachy")]
+    [SerializeField]
+    GameObject _dialogueList;
+
+    [SerializeField]
+    [Tooltip(@"This object will be shown as the end of each Dialogue message, 
+        Can be used to notify the user how to advance the messages.")]
+    GameObject _endOfMessagePrompt;
+
+    [Header("Configuration")]
+    [Tooltip(@"Controls the amount of time to pass before the next 
+        character will be displayed")]
+    [SerializeField]
+    float _textSpeed = 0.025f;
+
+    // Dialogue messages to display
+    Dialogue[] _dialogueArray;
+
+    // Current dialogue message, within the array
+    int _currentMessageItr;
+
+    // Cached GameObject properties
+    GameObject _gameObject;
+
+    public float textSpeed
+    {
+        get
+        {
+            return _textSpeed;
+        }
+    }
+
+    public void Awake()
+    {
+        // Cache GameObject properties
+        _gameObject = gameObject;
+
+        // Initialise dialogue list array
+        if (_dialogueList == null ||
+            _dialogueList.transform.childCount < 1)
+        {
+            Debug.LogError(string.Format(@"({0}) Dialogue list not set correctly. 
+                    Please ensure there is at least 1 child of the dialogue object",
+                this.name));
+        }
+        else
+        {
+            // Get array of Dialogue
+            GetDialogueList();
+
+            // Ensure dialogue list is valid
+            if (_dialogueArray == null ||
+                _dialogueArray.Length <= 0)
+            {
+                Debug.LogWarning(string.Format("({0}) Unable to find any dialogue messages", this.name));
+            }
+        }
+    }
+
+    void GetDialogueList()
+    {
+        // Initiialise temporary list of all the dialogue messages
+        List<Dialogue> dialogues =
+            new List<Dialogue>(_dialogueList.transform.childCount);
+
+        for (int i = 0; i < dialogues.Capacity; ++i)
+        {
+            // Get current child
+            Transform dialogueTransform = _dialogueList.transform.GetChild(i);
+
+            // Get Dialogue component, and save it to the list
+            Dialogue dialogue = dialogueTransform.GetComponent<Dialogue>();
+            dialogues.Add(dialogue);
+        }
+
+        // Save list as arry
+        _dialogueArray = dialogues.ToArray();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnEnable()
+    {
+        // Reset meessage interator, and show first message
+        _currentMessageItr = 0;
+        _dialogueArray[0].gameObject.SetActive(true);
+
+        // Hide End of Dialogue Prompt, if any exist
+        if (_endOfMessagePrompt)
+        {
+            _endOfMessagePrompt.SetActive(false);
+        }
+    }
+
+    public void OnDisable()
+    {
+        // Hide all dialogue messages, so they correctly reset
+        for (int i = 1; i < _dialogueArray.Length; ++i)
+        {
+            _dialogueArray[i].gameObject.SetActive(false);
+        }
+    }
+
+    public void ShowNextMessage()
+    {
+        // Ignore if this component is disabled
+        if (!this.enabled)
+        {
+            return;
+        }
+
+        // Show full message of current message, if still being animated
+        if (!_dialogueArray[_currentMessageItr].fullMessageShown)
+        {
+            _dialogueArray[_currentMessageItr].ShowFullMessage();
+            return;
+        }
+
+        // Hide current message
+        _dialogueArray[_currentMessageItr].gameObject.SetActive(false);
+        // Hide End of Dialogue Prompt
+        if (_endOfMessagePrompt != null)
+        {
+            DisplayEndOfDialoguePrompt();
+        }
+
+        _currentMessageItr++;
+
+        if (_currentMessageItr >= _dialogueArray.Length)
+        {
+            // End of messages, hide Dialogue system
+            _gameObject.SetActive(false);
+        }
+        else
+        {
+            // Show next message
+            _dialogueArray[_currentMessageItr].gameObject.SetActive(true);
+
+            HideEndOfDialoguePrompt();
+        }
+    }
+
+    public void DisplayEndOfDialoguePrompt()
+    {
+        // Display end of message prompt, if any exist
+        if (_endOfMessagePrompt != null)
+        {
+            _endOfMessagePrompt.SetActive(true);
+        }
+    }
+
+    void HideEndOfDialoguePrompt()
+    {
+        // Display end of message prompt, if any exist
+        if (_endOfMessagePrompt != null)
+        {
+            _endOfMessagePrompt.SetActive(false);
+        }
+    }
+}
